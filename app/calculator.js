a6ba0a9ce9d38657c2ed972aca45209669fe2dc6
axferox <axferox@gmail.com>
2021-12-30 04:40:05 +0300

    Fixed comments

--- app/calculator.js
+++ app/calculator.js
@@ -1,63 +1,56 @@
 /**
  *
- * A simple calss which performs the sumarize and multiply actions
+ * A simple class which performs the summarize and multiply actions
  * @class Calculator
  */
+
 class Calculator {
   /**
    * Creates an instance of Calculator.
-   * @param {*} number is the first incoming value for the calculations
-   * @param {*} increment is the second incoming value for summarize action
-   * @param {*} multiplier is the second incoming value for the multiply action
+   * @param {*} numbers is the incoming value for the calculations
    * @memberof Calculator
    */
-  constructor(number, increment, multiplier) {
-    this.number = number;
-    this.increment = increment;
-    this.multiplier = multiplier;
+  constructor(numbers) {
+    this.number = numbers;
   }
 
   /**
    *
    *
-   * @param {*} number is the first incoming value for the calculations
-   * @param {*} increment is the second incoming value for summarize action
-   * @return {*} returns the summ result of number and increment
+   * @param {*} numbers is the first incoming value for the calculations
+   * @return {*} returns the sum result of number and increment
    * @memberof Calculator
    */
-  add(number, increment) {
-    if (typeof (number) != 'number') {
-      // eslint-disable-next-line max-len
-      throw new TypeError(`The first value [${number}] is \"${typeof(number)}\", expected value: \"number\"`);
-    } else if (typeof (increment) != 'number') {
-      // eslint-disable-next-line max-len
-      throw new TypeError(`The second value [${increment}] is \"${typeof(increment)}\", expected value: \"number\"`);
-    } else {
-      return number + increment;
-    }
-  }
+  add(...numbers) {
+    return numbers.reduce((sum, current) => {
+      if (typeof (current) != 'number' || typeof (sum) != 'number') {
+        // eslint-disable-next-line max-len
+        throw new TypeError(`An entered value [${current}] is not a: \"number\"`);
+      } else {
+        return sum + current;
+       }
+      }, 0)
+   };
 
   /**
    *
    *
-   * @param {*} number is the first incoming value for the calculations
-   * @param {*} multiplier is the second incoming value for multiply action
-   * @return {*} returns the summ result of number and increment
+   * @param {*} numbers is the first incoming value for the calculations
+   * @return {*} returns the sum result of number and increment
    * @memberof Calculator
    */
-  multiply(number, multiplier) {
-    if (typeof (number) != 'number') {
-      // eslint-disable-next-line max-len
-      throw new TypeError(`The first value [${number}] is \"${typeof(number)}\", expected value: \"number\"`);
-    } else if (typeof (multiplier) != 'number') {
-      // eslint-disable-next-line max-len
-      throw new Error(`The second value [${multiplier}] is \"${typeof(multiplier)}\", expected value: \"number\"`);
-    } else {
-      return number * multiplier;
-    }
+  multiply(...numbers) {
+    return numbers.reduce((multiply, current) => {
+      if (typeof (current) != 'number' || typeof (multiply) != 'number') {
+        // eslint-disable-next-line max-len
+        throw new TypeError(`An entered value [${current}] is not a: \"number\"`);
+      } else {
+        return multiply * current;
+      }
+    })
   }
 }
 
-module.exports = Calculator;
+module.exports = { Calculator };
 
 
---
+++ test/specs/calculator.add.multiply.spec.js
@@ -0,0 +1,158 @@
+const {Calculator} = require('../../app/calculator');
+const {expect} = require('chai');
+
+// eslint-disable-next-line max-len
+describe(`It should test Calculator class method 'add' positive scenarios`, () => {
+    let calculator;
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+    afterEach(() => {
+        calculator = null;
+    });
+    it(`It shout test the calculation of the natural positive numbers`, () => {
+        expect(calculator.add(865, 45, 239)).to.be.equal(1149);
+    });
+    it(`It should test the calculation with the negative number`, () => {
+        expect(calculator.add(-356, 125)).to.be.equal(-231);
+    });
+    it(`It should test the calculation with whole numbers`, () => {
+        expect(calculator.add(548, 0)).to.be.equal(548);
+    });
+    it(`It should test the calculation with fractions`, () => {
+        expect(calculator.add(3 / 8, 4 / 8)).to.be.equal(7 / 8);
+    });
+    it(`It should test the calculation with the rational numbers`, () => {
+        // eslint-disable-next-line max-len
+        expect(calculator.add(-245.969, 138.354)).to.be.equal(-107.61499999999998);
+    });
+});
+
+// eslint-disable-next-line max-len
+describe(`It should test Calculator class method 'add' negative scenarios`, () => {
+    let calculator;
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+    afterEach(() => {
+        calculator = null;
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test that if 'number' is null an error is thrown`, () => {
+        expect(() => calculator.add(null, 5, 6)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'increment' is not defined(null) an error is thrown`, () => {
+        expect(() => calculator.add(68, null)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' and 'increment' are not defined(null) an error is thrown`, () => {
+        expect(() => calculator.add(null, null)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' and 'increment' are not defined(values are empty) an error is thrown`, () => {
+        expect(() => calculator.add()()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the only 'number' is defined an error is thrown`, () => {
+        expect(() => calculator.add(5)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the only 'number' is defined(null) an error is thrown`, () => {
+        expect(() => calculator.add(null)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the only 'number' is defined(as undefined) an error is thrown`, () => {
+        expect(() => calculator.add(undefined)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' and 'increment' are not defined(undefined) an error is thrown`, () => {
+        expect(() => calculator.add(undefined, undefined)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' is not defined(undefined) an error is thrown`, () => {
+        expect(() => calculator.add(undefined, 5)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'increment' is not defined(undefined) an error is thrown`, () => {
+        expect(() => calculator.add(68, undefined)()).to.throw();
+    });
+});
+
+// eslint-disable-next-line max-len
+describe(`It should test Calculator class method 'multiply' positive scenarios`, () => {
+    let multiplier;
+    beforeEach(() => {
+        multiplier = new Calculator();
+    });
+    afterEach(() => {
+        multiplier = null;
+    });
+    it(`It shout test the calculation of the natural positive numbers`, () => {
+        expect(multiplier.multiply(865, 45)).to.be.equal(38925);
+    });
+    it(`It should test the calculation with the negative number`, () => {
+        expect(multiplier.multiply(-356, 125)).to.be.equal(-44500);
+    });
+    it(`It should test the calculation with whole numbers`, () => {
+        expect(multiplier.multiply(548, 0)).to.be.equal(0);
+    });
+    it(`It should test the calculation with fractions`, () => {
+        expect(multiplier.multiply(3 / 8, 4 / 8)).to.be.equal(0.1875);
+    });
+    it(`It should test the calculation with the rational numbers`, () => {
+        // eslint-disable-next-line max-len
+        expect(multiplier.multiply(-245.969, 138.354)).to.be.equal(-34030.795026);
+    });
+});
+
+// eslint-disable-next-line max-len
+describe(`It should test Calculator class method 'multiply' negative scenarios`, () => {
+    let multiplier;
+    beforeEach(() => {
+        multiplier = new Calculator();
+    });
+    afterEach(() => {
+        multiplier = null;
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' is not defined(null) an error is thrown`, () => {
+        expect(() => multiplier.multiply(null, 5)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'increment' is not defined(null) an error is thrown`, () => {
+        expect(() => multiplier.multiply(68, null)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' and 'increment' are not defined(null) an error is thrown`, () => {
+        expect(() => multiplier.multiply(null, null)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' and 'increment' are not defined(values are empty) an error is thrown`, () => {
+        expect(() => multiplier.multiply()()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the only 'number' is defined an error is thrown`, () => {
+        expect(() => multiplier.multiply(5)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the only 'number' is defined(null) an error is thrown`, () => {
+        expect(() => multiplier.multiply(null)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the only 'number' is defined(as undefined) an error is thrown`, () => {
+        expect(() => multiplier.multiply(undefined)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' and 'increment' are not defined(undefined) an error is thrown`, () => {
+        expect(() => multiplier.multiply(undefined, undefined)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'number' is not defined(undefined) an error is thrown`, () => {
+        expect(() => multiplier.multiply(undefined, 5)()).to.throw();
+    });
+    // eslint-disable-next-line max-len
+    it(`It should test the 'increment' is not defined(undefined) an error is thrown`, () => {
+        expect(() => multiplier.multiply(68, undefined)()).to.throw();
+    });
+});
